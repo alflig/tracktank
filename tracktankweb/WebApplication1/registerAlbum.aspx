50d8d00d77c022698919209af7161cf1d87a5be3
Alf <alf_lig@hotmail.com>
2020-02-13 14:09:11 +0100

    registrer album
    Registrer album er lagt til men ikke helt ferdig. Den legger til dato, men ikke helt riktig

---
+++ tracktankweb/WebApplication1/registerAlbum.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="registerAlbum.aspx.cs" Inherits="WebApplication1.registerAlbum" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <p>Choose artist</p>
+          <asp:DropDownList ID = "artists" runat="server">
+</asp:DropDownList>
+            
+            
+            <p>Album label</p>
+            <asp:TextBox ID="albumName" Text="" runat="server" />
+            
+            <p>Number of tracks</p>
+            <asp:TextBox ID="numberOfTracks" Text="" runat="server" /> 
+           
+            
+            <p>Release Year</p>
+            <asp:TextBox ID="releaseYear" runat="server"></asp:TextBox>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+           
+           
+
+            <asp:Button ID="registerButton" Text="REGISTER" runat="server" OnClick="RegisterEventMethod" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ tracktankweb/WebApplication1/registerAlbum.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication1
+{
+    public partial class registerAlbum : System.Web.UI.Page
+    {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        MySql.Data.MySqlClient.MySqlCommand cmd;
+        String queryStr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                string constr = ConfigurationManager.ConnectionStrings["WebApplication1ConnectionString"].ConnectionString;
+                using (MySqlConnection con = new MySqlConnection(constr))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("SELECT artistID, Name FROM artist"))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = con;
+                        con.Open();
+                        artists.DataSource = cmd.ExecuteReader();
+                        artists.DataTextField = "name";
+                        artists.DataValueField = "artistID";
+                        artists.DataBind();
+                        con.Close();
+                    }
+                }
+                artists.Items.Insert(0, new ListItem("--Select Customer--", "0"));
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            releaseYear.Text = Calendar1.SelectedDate.ToString("dd/MM/yyyy");
+        }
+
+        protected void RegisterEventMethod(object sender, EventArgs e)
+        {
+            RegisterUser();
+        }
+        private void RegisterUser()
+        {
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebApplication1ConnectionString"].ToString();
+            conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+            conn.Open();
+            queryStr = "";
+            queryStr = "INSERT INTO webapplication1.album (artistID, albumLabel, numberOfTracks, releaseYear)" + "VALUES('" + artists.Text + "', '" + albumName.Text + "', '" + numberOfTracks.Text + "','" + releaseYear.Text + "')";
+            cmd = new MySql.Data.MySqlClient.MySqlCommand(queryStr, conn);
+            cmd.ExecuteReader();
+            conn.Close();
+        }
+
+
+
+    }
+}
